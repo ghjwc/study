46f2616074d8c36ea4a8123056c6547b91c117f6
jwc <612346@naver.com>
2022-06-13 11:07:59 +0900

    test

--- alert5.js
+++ alert5.js
@@ -14,14 +14,16 @@
 // }
 
 
-// let n = parseInt(prompt('숫자를 입력하세요.' , ''));
-// let sum = 0;
+let n = parseInt(prompt('숫자를 입력하세요.' , ''));
+let sum = 0;
 
-// for (let i = 1; i <= n; i++) {
-//     sum += i;
-// }
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
 
-// console.log(sum);
+console.log(sum);
+
+
 
 let login;
 
