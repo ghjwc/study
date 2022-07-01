1afa4ba116f5aad75303a49027d4d3362c7dd120
jwc <612346@naver.com>
2022-07-01 16:51:00 +0900

    0701

---
+++ jsStudy/project02/script.js
@@ -0,0 +1,14 @@
+let inputNum = document.querySelector('#inputNum');
+//     resultNum = parseInt(inputNum.value);
+
+// console.log(resultNum);
+
+const numBtn = document.querySelectorAll('.num');
+let index = 0;
+
+numBtn[index].addEventListener('click', function (e) {
+    for (let )
+
+    console.log(this.innerText);
+    inputNum.value = this.value;
+});
