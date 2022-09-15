cf3b77cad73551cafd2dd1b9abeabc72b216a933
jwc <612346@naver.com>
2022-09-15 17:26:17 +0900

    0915

--- sql_php/woobi/www/page/main.js
+++ sql_php/woobi/www/page/main.js
@@ -10,7 +10,7 @@ if (hambugger) {
     
         if (!mobileMenu.clientHeight) {
             mobileMenu.style.minHeight = 'calc(100vh - 6rem)';
-            mobileMenu.style.opacity = '0.5';
+            mobileMenu.style.opacity = '1';
             mobileMenu.style.visibility = 'visible';
             mobileMenu.style.transition = '0.5s ease';
             
@@ -49,20 +49,16 @@ window.addEventListener('scroll', () => {
 
     lis.forEach((li) => {
         li.style.background = 'transparent';
+        li.style.color = 'black';
     });
 
     secs.forEach((sec) => {
         if (sec.offsetTop <= window.scrollY && (sec.offsetTop + sec.clientHeight) >= window.scrollY) {
-            lis[index].style.background = 'red';
+            lis[index].style.background = 'black';
+            lis[index].style.color = 'white';
         }
         index++;
     });
-});
-
-// top
-const topBtn = document.getElementById('topBtn');
-topBtn.addEventListener('click', () => {
-    window.scrollTo({top:0, left:0, behavior:'smooth'});
 });
 
 // typeit
@@ -70,35 +66,65 @@ new TypeIt("#typeit", {
     speed: 70,
     startDelay: 900,
   })
-.type("개발자", { delay: 100 })
-// .move(null, { to: "START", instant: true, delay: 300 })
-// .move(1, { delay: 200 })
-// .delete(1)
-// .type("T", { delay: 225 })
-.pause(200)
-// .move(2, { instant: true })
-// .pause(200)
-// .move(5, { instant: true })
-// .move(5, { delay: 200 })
-// .type("a", { delay: 350 })
-.move(null, { to: "END" })
-.type("를")
-// .move(-4, { delay: 150 })
-.type(" 꿈꾸는")
-// .move(null, { to: "END" })
-.type(' <span class="myName">최지원</span>', { delay: 400 })
-// .delete(".place", { delay: 800, instant: true })
-.type('입니다', {
-    speed: 100,
-})
-.move(null, { to: "START" }, { delay: 100 })
-.type("<strong>프론트엔드</strong> ", { delay: 500 })
-.move(null, { to: "END" })
-.type(".", { delay: 100 })
-.go();
+    .type("개발자", { delay: 100 })
+    // .move(null, { to: "START", instant: true, delay: 300 })
+    // .move(1, { delay: 200 })
+    // .delete(1)
+    // .type("T", { delay: 225 })
+    .pause(200)
+    // .move(2, { instant: true })
+    // .pause(200)
+    // .move(5, { instant: true })
+    // .move(5, { delay: 200 })
+    // .type("a", { delay: 350 })
+    .move(null, { to: "END" })
+    .type("를")
+    // .move(-4, { delay: 150 })
+    .type(" 꿈꾸는")
+    // .move(null, { to: "END" })
+    .type(' <span class="myName">최지원</span>', { delay: 400 })
+    // .delete(".place", { delay: 800, instant: true })
+    .type('입니다', {
+        speed: 100,
+    })
+    .move(null, { to: "START" }, { delay: 100 })
+    .type("<strong>프론트엔드</strong> ", { delay: 500 })
+    .move(null, { to: "END" })
+    .type(".", { delay: 100 })
+    .go();
 
-// down button
-const downBtn = document.querySelector('.aboutImg > img');
+
+
+// button
+const downBtn = document.querySelector('.firstImg > img'),
+        topBtn = document.querySelector('.topBtn > img'),
+        bottomBtn = document.querySelector('.bottomBtn > img');
+        
 downBtn.addEventListener('click', () => {
-    window.scrollTo({top:(headerBack.scrollHeight + secs[0].scrollHeight), behavior:"smooth"});
+    window.scrollTo({top:(headerBack.scrollHeight + document.querySelector('.first').scrollHeight), behavior:"smooth"});
+});
+
+topBtn.addEventListener('click', () => {
+    window.scrollTo({top:0, left:0, behavior:'smooth'});
+});
+
+bottomBtn.addEventListener('click', () => {
+    window.scrollTo({top:document.body.scrollHeight, left:0, behavior:'smooth'});
+});
+
+ScrollOut({
+    targets: ".title",
+    threshold: 0.5,
+    once: false
+});
+
+// menu click
+const headerList = document.querySelectorAll('.headerList > li');
+headerList.forEach((li) => {
+    li.addEventListener('click', () => {
+        for (let i = 0; i < secs.length; i++) {
+            console.log(secs[i]);
+            window.scrollTo({top:secs[i].scrollHeight});
+        }
+    });
 });
