a981299d8e31c80fb7229efd19876564ce06ac5b
jwc <612346@naver.com>
2022-09-15 14:05:53 +0900

    0915

--- sql_php/woobi/www/page/main.js
+++ sql_php/woobi/www/page/main.js
@@ -10,9 +10,16 @@ if (hambugger) {
     
         if (!mobileMenu.clientHeight) {
             mobileMenu.style.minHeight = 'calc(100vh - 6rem)';
-            mobileMenu.style.opacity = '1';
+            mobileMenu.style.opacity = '0.5';
             mobileMenu.style.visibility = 'visible';
             mobileMenu.style.transition = '0.5s ease';
+            
+            if (headerBack.style.opacity == '1') {
+                mobileMenu.style.opacity = '1';   
+            } else if (headerBack.style.opacity == '0.5') {
+                mobileMenu.style.opacity = '0.5';
+            }
+
         } else {
             mobileMenu.style.minHeight = '0';
             mobileMenu.style.opacity = '0';
@@ -30,8 +37,12 @@ let secs = document.querySelectorAll('.section'),
 
 window.addEventListener('scroll', () => {
     headerBack.style.opacity = '0.5';
+    headerBack.style.transition = '0.5s ease';
+    mobileMenu.style.opacity = '0.5';
     if (window.scrollY == 0) {
         headerBack.style.opacity = '1';
+        headerBack.style.transition = '0.5s ease';
+        mobileMenu.style.opacity = '1';
     }
 
     let index = 0;
@@ -53,4 +64,41 @@ const topBtn = document.getElementById('topBtn');
 topBtn.addEventListener('click', () => {
     window.scrollTo({top:0, left:0, behavior:'smooth'});
 });
-    
+
+// typeit
+new TypeIt("#typeit", {
+    speed: 70,
+    startDelay: 900,
+  })
+.type("개발자", { delay: 100 })
+// .move(null, { to: "START", instant: true, delay: 300 })
+// .move(1, { delay: 200 })
+// .delete(1)
+// .type("T", { delay: 225 })
+.pause(200)
+// .move(2, { instant: true })
+// .pause(200)
+// .move(5, { instant: true })
+// .move(5, { delay: 200 })
+// .type("a", { delay: 350 })
+.move(null, { to: "END" })
+.type("를")
+// .move(-4, { delay: 150 })
+.type(" 꿈꾸는")
+// .move(null, { to: "END" })
+.type(' <span class="myName">최지원</span>', { delay: 400 })
+// .delete(".place", { delay: 800, instant: true })
+.type('입니다', {
+    speed: 100,
+})
+.move(null, { to: "START" }, { delay: 100 })
+.type("<strong>프론트엔드</strong> ", { delay: 500 })
+.move(null, { to: "END" })
+.type(".", { delay: 100 })
+.go();
+
+// down button
+const downBtn = document.querySelector('.aboutImg > img');
+downBtn.addEventListener('click', () => {
+    window.scrollTo({top:(headerBack.scrollHeight + secs[0].scrollHeight), behavior:"smooth"});
+});
