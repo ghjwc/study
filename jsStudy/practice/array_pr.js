8e02c13d4c4d0223e9c23a8b54cf146b9d9f6df9
jwc <612346@naver.com>
2022-09-23 14:49:48 +0900

    0923

---
+++ jsStudy/practice/array_pr.js
@@ -0,0 +1,8 @@
+let numDiv = document.querySelector('.numDiv'),
+    numClick = document.querySelector('.numClick'),
+    increase = 0;
+
+numClick.addEventListener('click', () => {
+    numDiv.innerHTML++;
+    
+})
